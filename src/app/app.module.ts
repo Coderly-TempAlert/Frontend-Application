1e234a1162bb6bf482ceea95f413904f855e0f20
Mathyu Cardozo <xmathyu@gmail.com>
2023-04-23 20:40:29 -0500

    Dashboards component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoresComponent } from './modules/dashboards/stores/stores.component';
+import { StockComponent } from './modules/dashboards/stock/stock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StoresComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
