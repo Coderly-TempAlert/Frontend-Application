cda6b27f0863a2e3359dd0f87ac519e45dab7c8c
Eleazar Braco Terry <ebracoterry@gmail.com>
2023-04-24 22:43:00 -0500

    feat: stores done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,32 @@ import { StockComponent } from './modules/dashboards/stock/stock.component';
 import { SidenavComponent } from './modules/dashboards/sidenav/sidenav.component';
 import { DashboardsComponent } from './modules/dashboards/dashboards.component';
 
+
 // ANGULAR MATERIAL
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { MatOptionModule } from '@angular/material/core';
+import {  MatSelectModule } from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { CardComponent } from './components/card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './components/emp-add-edit/emp-add-edit.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -20,13 +44,34 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     StoresComponent,
     StockComponent,
     SidenavComponent,
-    DashboardsComponent
+    DashboardsComponent,
+    CardComponent,
+    EmpAddEditComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { ViewEncapsulation } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    ngZoneEventCoalescing: true,
+    ngZoneRunCoalescing: true,
+    preserveWhitespaces: false,
+  })
+  .catch((err) => console.error(err));
