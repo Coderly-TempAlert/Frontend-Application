89a59282caa5c0b61064a40f105a3a6a3e65e56e
Mathyu Cardozo <xmathyu@gmail.com>
2023-05-06 14:56:39 -0500

    Build

--- src/app/core/models/store.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Store {
-    id: string;
-    name: string;
-    address: string;
-    description: string;
-    createdDate: string;
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  url_api: 'https://tempalert-business.herokuapp.com/api',
-};
